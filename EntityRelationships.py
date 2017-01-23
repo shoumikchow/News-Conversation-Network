dfa8b77de7e16d146b2247f0ef1be20153e8b886
Niamat Zawad <zawad1879@gmail.com>
2017-01-23 19:08:18 +0600

    Added comment determine no. of rows

--- EntityRelationships.py
+++ EntityRelationships.py
@@ -11,6 +11,7 @@ with open('dhaka_tribune.csv', 'r') as f:
     counter = 0
     rownum = 0
     for row in reader:
+        #determine number of rows in csv to be analysed
         if counter == 1:
             break
 
