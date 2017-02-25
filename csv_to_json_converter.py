f604c61a05e4077d015f41060f470dd33aeaed0f
Niamat Zawad <zawad1879@gmail.com>
2017-02-26 02:53:26 +0600

    Fixed some bugs. Each node connected to predefined keyword nodes

--- csv_to_json_converter.py
+++ csv_to_json_converter.py
@@ -2,43 +2,55 @@ import csv
 import json
 
 csvfile = open('./Scraped data/network_direct_improvised.csv', 'r')
-jsonfile = open('../output_two_hops.json', 'w')
+jsonfile = open('output_two_hops.json', 'w')
 
-def find_edges(reader,subject):
-    print (subject)
+def find_second_hop_edges(subject):
+
     coun = 0
-    for row in reader:
-        #print (row["subject"])
+    csvfil = open('./Scraped data/network_direct_improvised.csv', 'r')
+    read = csv.DictReader(csvfil)
+    second_hop_objects_already_added = []
+    for row in read:
         if coun == 10:
             break
     
-        if row["subject"] == subject:
-            print ("got subject")
-            arr = {"source":row["subject"],"target":row["object"]}
-            #list_of_objects.append(row["object"])
-            json.dump(arr, jsonfile)
-            jsonfile.write(',\n')
-    
-            coun += 1
+        if row["subject"] == subject and row["object"] in keywords:
+            if row["object"] not in second_hop_objects_already_added:
+                #print (row["subject"]+" "+row["object"])
+                second_hop_objects_already_added.append(row["object"])
+                arr = {"source":row["subject"],"target":row["object"]}
+                json.dump(arr, jsonfile)
+                jsonfile.write(',\n')
+                coun += 1
+
+
 
 count = 0
 keywords = ["Dhaka", "Bangladesh", "India", "Chittagong", "Pakistan", "Myanmar", "Narayanganj", "Malaysia", "Gazipur", "Sylhet", "China", "Barisal", "Khulna",
 "BNP", "Dhaka Tribune", "Awami League", "Jamaat", "RAB", "EC", "BGB", "Election Commission", "High Court", "Chhatra League", "Jatiya Party", "ACC", "Shibir", "JMB", "DMP",
 "Sheikh Hasina", "Khaleda Zia", "Ershad", "Fakhrul", "Monirul", "Tarique Rahman", "Muhith", "Nuzami", "Bangabandhu", "Ziaur Rahman", "Modi"]
 
+objects_already_added = []
+
 reader = csv.DictReader(csvfile)
-for row in reader:
+for row_number, row in enumerate(reader):
+
     if count == 10:
         break
     
     if row["subject"] == "Sheikh Hasina" and row["object"] in keywords:
+        if row["object"] not in objects_already_added:
+            #print ("Count: "+str(count)+" "+row["subject"]+" "+row["object"])
+            objects_already_added.append(row["object"])
+            #print ("---"+str(row_number)+"---")
+            #print (row["text"])
+            arr = {"source":row["subject"],"target":row["object"]}
+            find_second_hop_edges(row["object"])
+            #print ("Back!")
+            json.dump(arr, jsonfile)
+            jsonfile.write(',\n')
+            count += 1
 
-        arr = {"source":row["subject"],"target":row["object"]}
-        find_edges(reader,row["object"])
-        #list_of_objects.append(row["object"])
-        json.dump(arr, jsonfile)
-        jsonfile.write(',\n')
-        count += 1
 
 
 
