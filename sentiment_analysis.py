4e70a0ad3106d74f4dd86c078f5b35d71bd14a3f
shoumik <shoumikchow@gmail.com>
2017-02-26 05:38:35 +0600

    cleaned code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,7 @@
 import indicoio
 import csv
 
-indicoio.config.api_key = '441284b3f30c52691c44d9a8dbfbbd6b'
+indicoio.config.api_key = "my_api_key"
 
 
 def get_text_sentiment(text):
@@ -18,13 +18,12 @@ def get_text_sentiment(text):
 
 with open("./Scraped data/quotations_and_speeches_v2.0.csv", "r") as file:
     reader = csv.reader(file)
-    next(reader)
-    with open("./Scraped data/quotations_and_speeches_with_sentiment_v2.1.csv", "a") as out:
-        writer = csv.writer(out)
-        writer.writerow(["original_id", "timestamp", "text", "locations", "organizations", "persons", "original_tag", "naive_tag", "keywords", "sentiment", "sentiment_value"])
-
     for row in reader:
+        # print(row)
+        # break
         sentiment, sentiment_value = get_text_sentiment(row[2])
         with open("./Scraped data/quotations_and_speeches_with_sentiment_v2.1.csv", "a") as out:
             writer = csv.writer(out)
             writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], sentiment, sentiment_value])
+            print(count)
+            count += 1
