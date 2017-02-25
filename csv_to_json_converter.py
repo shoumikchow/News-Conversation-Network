cae496cfc2336807f1be3d48cf9bc24775584196
Niamat Zawad <zawad1879@gmail.com>
2017-02-26 00:57:34 +0600

    Displaying two hop network.

--- csv_to_json_converter.py
+++ csv_to_json_converter.py
@@ -2,19 +2,47 @@ import csv
 import json
 
 csvfile = open('./Scraped data/network_direct_improvised.csv', 'r')
-jsonfile = open('output.json', 'w')
+jsonfile = open('../output_two_hops.json', 'w')
 
+def find_edges(reader,subject):
+    print (subject)
+    coun = 0
+    for row in reader:
+        #print (row["subject"])
+        if coun == 10:
+            break
+    
+        if row["subject"] == subject:
+            print ("got subject")
+            arr = {"source":row["subject"],"target":row["object"]}
+            #list_of_objects.append(row["object"])
+            json.dump(arr, jsonfile)
+            jsonfile.write(',\n')
+    
+            coun += 1
 
-count = 1
+count = 0
+keywords = ["Dhaka", "Bangladesh", "India", "Chittagong", "Pakistan", "Myanmar", "Narayanganj", "Malaysia", "Gazipur", "Sylhet", "China", "Barisal", "Khulna",
+"BNP", "Dhaka Tribune", "Awami League", "Jamaat", "RAB", "EC", "BGB", "Election Commission", "High Court", "Chhatra League", "Jatiya Party", "ACC", "Shibir", "JMB", "DMP",
+"Sheikh Hasina", "Khaleda Zia", "Ershad", "Fakhrul", "Monirul", "Tarique Rahman", "Muhith", "Nuzami", "Bangabandhu", "Ziaur Rahman", "Modi"]
 
 reader = csv.DictReader(csvfile)
 for row in reader:
-    if count == 20:
+    if count == 10:
         break
-    arr = {"source":row["subject"],"target":row["object"]}
-    json.dump(arr, jsonfile)
-    jsonfile.write(',\n')
-    count += 1
+    
+    if row["subject"] == "Sheikh Hasina" and row["object"] in keywords:
+
+        arr = {"source":row["subject"],"target":row["object"]}
+        find_edges(reader,row["object"])
+        #list_of_objects.append(row["object"])
+        json.dump(arr, jsonfile)
+        jsonfile.write(',\n')
+        count += 1
 
 
 
+
+# Dhaka, Bangladesh, India, Chittagong, Pakistan, Myanmar, Narayanganj, Malaysia, Gazipur, Sylhet, China, Barisal, Khulna
+# BNP, Dhaka Tribune, Awami League, Jamaat, RAB, EC, BGB, Election Commission, High Court, Chhatra League, Jatiya Party, ACC, Shibir, JMB, DMP
+# Sheikh Hasina, Khaleda Zia, Ershad, Fakhrul, Monirul, Tarique Rahman, Muhith, Nuzami, Bangabandhu, Ziaur Rahman, Modi
