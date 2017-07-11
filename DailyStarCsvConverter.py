4342a38a704ecde95e9a80a7586dadfc659c66e6
zawad1879 <zawad1879@gmail.com>
2017-07-11 14:47:38 +0600

    Cleaned text. Final script

--- DailyStarCsvConverter.py
+++ DailyStarCsvConverter.py
@@ -7,7 +7,7 @@ def insertInCsv(uniqueNewsID,newsDate,line,allEntities):
 
     #Take combination pairs
     for instance in itertools.combinations(allEntities, 2):
-        print (instance)
+        #print (instance)
         with open("DailyStar.csv", "a") as my_output:
             writer = csv.writer(my_output)
             writer.writerow([uniqueNewsID, newsDate, instance[0], getTag(instance[0]), instance[1], getTag(instance[1]), line])
@@ -16,13 +16,15 @@ def insertInCsv(uniqueNewsID,newsDate,line,allEntities):
 
 def populateEntityArray(word,entityTypeArray):
     
-    [entity,tag] = word.split("/")
+    # if there's a word like Np/cl/ORGANIZATION, split() will give error
+    [entity,tag] = word.rsplit("/",1)
     global previousTag
 
     #If first time encountering the tag, append to array
-    if previousTag == '':
+    if tag != previousTag:
         entityTypeArray.append(entity)
 
+    print (entityTypeArray)
     #If previous word also had the same tag, assume current entity and previous one are same
     if previousTag == tag:
         entityTypeArray[-1] = entityTypeArray[-1]+" "+entity
@@ -63,43 +65,50 @@ uniqueNewsID = ''
 #Open raw dailyStar news text
 with open('DailyStar.txt') as f:
     content = f.readlines()
-    count = 0
+    count = 1
     #Read each line in the txt file
     for line in content:
-        uniqueNewsID = uuid.uuid4()
-        if count == 20:
-            break
+        print ("Line number is "+str(count))
 
         uniqueNewsID = uuid.uuid4()
 
         if re.search(r'^\d\d\d\d-\d\d-\d\d$',line):
             newsDate = line
-            
+            count += 1
             continue
 
         count += 1
-        print("This is the line "+line)
+        #print("This is the line "+line)
+
         #Separate each word in the line and place them in the array
         arr = line.split()
 
+        neatSentence = ''
         #Read each word in the line
-        for word in arr:
+        for index,word in enumerate(arr):
+
+            if word == '//O' or word == '//ORGANIZATION' or word == '//PERSON' or word == '//LOCATION':
+                continue
 
             #if the entity is an organization
             if re.search(r'[\dA-z]*\/ORGANIZATION',word):
                 populateEntityArray(word, organizations)
 
+            #if the entity is a location
             elif re.search(r'[\dA-z]*\/LOCATION',word):
                 populateEntityArray(word, locations)
 
+            #if the entity is a person
             elif re.search(r'[\dA-z]*\/PERSON',word):
                 populateEntityArray(word, persons)
 
             elif re.search(r'[\dA-z]*\/O',word):
                 previousTag = ''
+
+            neatSentence += word.split("/")[0] + ' '
         
         allEntities = organizations + persons + locations
-        insertInCsv(uniqueNewsID,newsDate,line,allEntities)
+        insertInCsv(uniqueNewsID,newsDate,neatSentence.strip(),allEntities)
         emptyAllLists()
         """print (organizations)
         print (persons)
