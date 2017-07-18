0227b26e1ea3285e598d39d55b0ed423135da599
zawad1879 <zawad1879@gmail.com>
2017-07-18 10:41:56 +0600

    Found histogram of frequency of keywords in DS

--- GetDegreesOfKeywordsInDailyStar.py
+++ GetDegreesOfKeywordsInDailyStar.py
@@ -1,9 +1,11 @@
 import csv
+import matplotlib.pyplot as plt
+import operator
 
 csvfile = open('../DailyStar.csv', 'r')
 csvreader = csv.reader(csvfile, delimiter=',')
 
-keywords = ['Sheikh Hasina', 'Hasina', 'Khaleda Zia', 'Khaleda', 'Zia', 'Ershad', 'Fakhrul', 'Tarique Rahman', 'Muhith', 'Nizami', 'Bangabandhu', 'Ziaur Rahman', 'Mirza Fakhrul Islam Alamgir', 'Obaidul Quader', 'Quader Molla', 'Ghulam Azam', 'Asaduzzaman Khan Kamal','BNP', 'Awami League', 'Jamaat', 'EC', 'Election Commission', 'Chhatra League', 'Jatiya Party', 'ACC', 'Shibir', 'BCL', 'Foreign Ministry', 'Jubo League', 'Chhatra Dal']
+keywords = ['Sheikh Hasina', 'Hasina', 'Khaleda Zia', 'Khaleda', 'Zia', 'Ershad', 'Fakhrul', 'Tarique Rahman','Fakhruddin Ahmed', 'Muhith', 'Nizami', 'Bangabandhu', 'Ziaur Rahman', 'Mirza Fakhrul Islam Alamgir', 'Obaidul Quader', 'Quader Molla', 'Ghulam Azam', 'Asaduzzaman Khan Kamal','BNP', 'Awami League', 'Jamaat','Jamaat-e-Islami', 'EC', 'Election Commission', 'Chhatra League', 'Jatiya Party', 'ACC', 'Shibir', 'BCL', 'Foreign Ministry', 'Jubo League', 'Chhatra Dal','Bangladesh Chhatra League','Asian Development Bank','Jahangirnagar University','Parliament','Shahbagh','Bhola','Padma Bridge','Supreme Court Bar Association','University of Dhaka','Bangabhaban']
 
 keywordDegree = {}
 
@@ -32,6 +34,8 @@ def findCommonSubstring(sub, obj):
 	else:
 		return False
 
+keywordInDict = []
+frequencyOfKeyword  = []
 
 initializeKeywordDegree()
 #Skip first line
@@ -53,6 +57,23 @@ for row in csvreader:
 		elif obj in keywords:
 			incrementDegreeOfKeyword(obj)
 
+keywordDegree = sorted(keywordDegree.items(), key=operator.itemgetter(1), reverse=True)
+
 for i in keywordDegree:
-	print (i+": "+str(keywordDegree[i]))
+	#print (i+": "+str(keywordDegree[i]))
+	print(i)
+	keywordInDict.append(i[0])
+	frequencyOfKeyword.append(i[1])
+
+#keywordInDict = range(0,42)
+#print (len(keywordInDict))
+"""plt.bar(keywordInDict, height= frequencyOfKeyword, width=1)
+plt.xlabel('Keywords')
+plt.ylabel('Frequency')
+plt.set_xticklabels(keywordInDict, rotation='vertical', fontsize=18) 
+plt.yticks(range(0, max(frequencyOfKeyword), 1000))"""
+
+plt.bar(range(len(frequencyOfKeyword)), frequencyOfKeyword, align='center',width=1)
+plt.xticks(range(len(frequencyOfKeyword)), keywordInDict, size='small',rotation='vertical')
+plt.show()
 
