25005c1b5f248d3577dd75bc0e2a96afa3a06373
zawad1879 <zawad1879@gmail.com>
2017-08-14 19:16:09 +0600

    Added code to find time ranges when events occured

--- GetEdgeOccurences.py
+++ GetEdgeOccurences.py
@@ -2,14 +2,14 @@ import csv
 from collections import Counter
 import matplotlib.pyplot as plt
 import datetime
-#csvfile = open('../person_count.csv', 'r')
+
 csvfile = open('DailyStar.csv', 'r')
 csvreader = csv.reader(csvfile, delimiter=',')
 
 """keywords = []
 for row in csvreader:
-	keywords.append(row[0])
-print(keywords)"""
+    keywords.append(row[0])
+print(keywords)"""  
 
 #indirect_network.csv
 """csvfile = open('../network_indirect.csv', 'r')
@@ -19,36 +19,36 @@ csvreader = csv.reader(csvfile, delimiter=',')"""
 csvHartalFile = open('../../data-strike-IGC.csv', 'r')
 csvHartalReader = csv.reader(csvHartalFile, delimiter=',')
 
-keywords = ['Sheikh Hasina', 'Hasina', 'Khaleda Zia', 'Khaleda', 'Zia', 'Ershad', 'Fakhrul', 'Tarique Rahman','Fakhruddin Ahmed', 'Muhith', 'Nizami', 'Bangabandhu', 'Ziaur Rahman', 'Mirza Fakhrul Islam Alamgir', 'Obaidul Quader', 'Quader Molla', 'Ghulam Azam', 'Asaduzzaman Khan Kamal','BNP', 'Awami League', 'Jamaat','Jamaat-e-Islami', 'EC', 'Election Commission', 'Chhatra League', 'Jatiya Party', 'ACC', 'Shibir', 'BCL', 'Foreign Ministry', 'Jubo League', 'Chhatra Dal','Bangladesh Chhatra League','Asian Development Bank','Jahangirnagar University','Parliament','Shahbagh','Bhola','Padma Bridge','Supreme Court Bar Association','University of Dhaka','Bangabhaban']
+keywords = ['Sheikh Hasina', 'Hasina', 'Khaleda Zia', 'Khaleda', 'Zia', 'Ershad', 'Fakhrul', 'Tarique Rahman','Fakhruddin Ahmed', 'Muhith', 'Nizami', 'Bangabandhu', 'Ziaur Rahman', 'Mirza Fakhrul Islam Alamgir', 'Obaidul Quader', 'Quader Molla', 'Ghulam Azam', 'Asaduzzaman Khan Kamal','BNP', 'Awami League', 'Jamaat','Jamaat-e-Islami', 'EC', 'Election Commission', 'Chhatra League', 'Jatiya Party', 'ACC', 'Shibir', 'BCL', 'Foreign Ministry', 'Jubo League', 'Chhatra Dal','Bangladesh Chhatra League','Asian Development Bank','Jahangirnagar University','Parliament','Shahbagh','Bhola','Padma Bridge','Supreme Court Bar Association','University of Dhaka','Bangabhaban','Caretaker','Sahara Khatun','Shah AMS Kibria','President Ahmed','Jamaat-ul-Mujahideen','Jagrata Muslim Janata Bangladesh','Hizb-ut Tahrir','Motiur Rahman Nizami','Abul Kalam Azad','Ghulam Azam','Abdul Kader Mullah','Mir Quasem Ali','Abdus Subhan','Ansarullah Bangla Team','Shia Muslims','Ali Ahsan Mohammad Mujahid','Salahuddin Quader Chowdhury','atheists','Ahmed Rajib Haider','Rohingya','refugee']
 
 #Check if the two entites might share a common substring
 def findCommonSubstring(sub, obj):
-	shortString = ""
-	longString = ""
-	if len(sub)<len(obj):
-		shortString = sub
-		longString = obj
-	else:
-		shortString = obj
-		longString = sub
-	if shortString in longString:
-		#Making the assumption of 3 since the shortest name possible in the dataset is Zia
-		if len(shortString)>3:
-			return True
-		else:
-			return False
-	else:
-		return False
+    shortString = ""
+    longString = ""
+    if len(sub)<len(obj):
+        shortString = sub
+        longString = obj
+    else:
+        shortString = obj
+        longString = sub
+    if shortString in longString:
+        #Making the assumption of 3 since the shortest name possible in the dataset is Zia
+        if len(shortString)>3:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 #Format date which conforms with the timestamp in indirect_network.csv
 def dateFormatter(year,month,day):
-	if len(month) == 1:
-		month = "0"+month
-	if len(day) == 1:
-		day = "0"+day
+    if len(month) == 1:
+        month = "0"+month
+    if len(day) == 1:
+        day = "0"+day
 
-	date = year+"-"+month+"-"+day
-	return date
+    date = year+"-"+month+"-"+day
+    return date
 
 subject_objects_for_ef_idf = []
 
@@ -70,57 +70,55 @@ next(csvreader)
 
 #Find out edge occurences
 for row in csvreader:
-	
-	#If the subject is a political entity and the subject is not talking to himself
-	if (row[2] in keywords or row[4] in keywords) and row[2] != row[4]:
-		
-		sub = (row[2])
-		obj = (row[4])
-		newsID = (row[0])
-		date = (row[1])
-		"""dateWithoutTime = date[:10]"""
-		dateWithoutTime = date
+    
+    #If the subject is a political entity and the subject is not talking to himself
+    if (row[2] in keywords or row[4] in keywords) and row[2] != row[4]:
+        
+        sub = (row[2])
+        obj = (row[4])
+        newsID = (row[0])
+        """dateWithoutTime = date[:10]"""
+        date = (row[1]).replace("\n","")
 
+        #Get only 2013 data since only 2013 hartal data is available
+        """if dateWithoutTime[:4] != "2013":
+            continue"""
+        
+        #Check whether subject object have common substring
+        if findCommonSubstring(sub,obj) == True:
+            continue
 
-		#Get only 2013 data since only 2013 hartal data is available
-		"""if dateWithoutTime[:4] != "2013":
-			continue"""
-		
-		#Check whether subject object have common substring
-		if findCommonSubstring(sub,obj) == True:
-			continue
+        """if tuple((sub,obj)) not in subject_objects_for_ef_idf:
+            subject_objects_for_ef_idf.append(tuple((sub,obj)))"""
 
-		"""if tuple((sub,obj)) not in subject_objects_for_ef_idf:
-			subject_objects_for_ef_idf.append(tuple((sub,obj)))"""
+        #Get political edge and add it to the list
+        subject_objects.append(tuple((sub,obj)))
+        #Get political edge with the timestamp and add it to another list
+        subject_objects_with_timestamps.append(tuple((sub,obj,date)))
 
-		#Get political edge and add it to the list
-		subject_objects.append(tuple((sub,obj)))
-		#Get political edge with the timestamp and add it to another list
-		subject_objects_with_timestamps.append(tuple((sub,obj,date)))
+        [year,month,day] = date.split("-")
+        
+        #DailyStarSpecific
+        if date == previousDay:
+            edgeOccurencesEachDay[-1] += 1
 
-		[year,month,day] = date.split("-")
-		
-		#DailyStarSpecific
-		if date == previousDay:
-			edgeOccurencesEachDay[-1] += 1
-
-		else:
-			edgeOccurencesEachDay.append(1)
-			days.append(datetime.datetime(int(year),int(month),int(day),0,0))
-			previousDay = date
+        else:
+            edgeOccurencesEachDay.append(1)
+            days.append(datetime.datetime(int(year),int(month),int(day),0,0))
+            previousDay = date
 
 #Print the edge occurences each day
 """for i in range(0,10):
-	print(days[i]+" "+str(edgeOccurencesEachDay[i]))
+    print(days[i]+" "+str(edgeOccurencesEachDay[i]))
 """
 """#Ignore the first row
 next(csvHartalReader)
 
 for row in csvHartalReader:
-	if row[0] != 2013:
-		continue
+    if row[0] != 2013:
+        continue
 
-	hartalDate = dateFormatter(row[0],row[1],row[2])"""
+    hartalDate = dateFormatter(row[0],row[1],row[2])"""
 
 
 #Find the number of occurences of each tuple
@@ -129,8 +127,8 @@ counter = Counter(subject_objects)
 #Convert the counter into a usable array
 numberOfTupleOccurences = sorted(counter.iteritems())
 
-"""for i in numberOfTupleOccurences:
-	print (i)"""
+# for i in numberOfTupleOccurences:
+#     print (i)
 #print(subject_objects[:1000])
 
 count = [0]
@@ -139,11 +137,11 @@ currentCount = 0
 
 #YAxis
 """for x in range(1,16):
-	for i in numberOfTupleOccurences:
-		if i[1] == x:
-			currentCount += 1
-	count.append(currentCount)
-	currentCount = 0
+    for i in numberOfTupleOccurences:
+        if i[1] == x:
+            currentCount += 1
+    count.append(currentCount)
+    currentCount = 0
 """
 
 #XAxis
@@ -156,32 +154,74 @@ plt.xlabel('Number of edge occurences')
 plt.ylabel('Unique edges')
 plt.yticks(range(0, max(count)+100, 1000))"""
 
+print (len(edgeOccurencesEachDay))
+print (len(days))
+
+counterTimestamp = Counter(subject_objects_with_timestamps)
+numberOfTupleOccurencesWithTimestamp = sorted(counterTimestamp.iteritems())
+
+"""for i in numberOfTupleOccurencesWithTimestamp:
+    if '2008-11' in i[0][2]:
+        print i"""
+"""for i in range(len(edgeOccurencesEachDay)):
+    print(str(days[i])+" "+str(edgeOccurencesEachDay[i]))"""
+
+
+##To get the time ranges when events occured
+eventsTimeArray = []
+eventsArray = []
+startEdgeLength = edgeOccurencesEachDay[0]
+startDate = days[0]
+
+for x in range(len(edgeOccurencesEachDay)):
+    
+    if startEdgeLength == 0:
+        #setting the threshold for edge occurences
+        if edgeOccurencesEachDay[x] < 20:
+            continue
+        else:
+            startEdgeLength = edgeOccurencesEachDay[x]
+            startDate = days[x]
+
+    if edgeOccurencesEachDay[x] <= startEdgeLength:
+        dateDifference = (days[x] - startDate).days
+        #setting date interval
+        if dateDifference > 15:
+            eventsTimeArray.append(startDate.strftime("%Y-%m-%d")+" "+days[x].strftime("%Y-%m-%d")+" "+str(dateDifference))
+            eventsArray.append(str(startEdgeLength)+" "+str(edgeOccurencesEachDay[x]))
+            if (x+1) < len(edgeOccurencesEachDay):
+                #startEdgeLength = edgeOccurencesEachDay[x+1]
+                startEdgeLength = 0
+                startDate = days[x+1]
+
+print(len(eventsArray))
+for i in range(len(eventsTimeArray)):
+    print(eventsTimeArray[i]+" "+eventsArray[i])
+
+
 #dailystarspecific
 plt.bar(days, height= edgeOccurencesEachDay, width=1)
 #plt.plot(days,edgeOccurencesEachDay)
-plt.xlabel('Number of edge occurences')
-plt.ylabel('Unique edges')
+plt.xlabel('Year')
+plt.ylabel('Total number of edges per day')
 #plt.yticks(range(0, max(edgeOccurencesEachDay)+100, 1000))
 #plt.gcf().autofmt_xdate()
 
-
+#print (edgeOccurencesEachDay)
 next(csvHartalReader)
 for row in csvHartalReader:
-	
-	year = row[0]
-	month = row[1]
-	day = row[2]
+    
+    year = row[0]
+    month = row[1]
+    day = row[2]
 
-	if int(year) < 2007:
-		continue
-	hartalDates.append(datetime.datetime(int(year),int(month),int(day),0,0))
-	hartalIndicator.append(60)
+    if int(year) < 2007:
+        continue
+
+    hartalDates.append(datetime.datetime(int(year),int(month),int(day),0,0))
+    hartalIndicator.append(60)
 
 plt.bar(hartalDates, height= hartalIndicator, width=1)
-
-
-
-
 
 plt.show()
 #print (numberOfTupleOccurences)
